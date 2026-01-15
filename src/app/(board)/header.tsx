8a04ec040addf514122f7dff713fb914f355ea4a
carlosalexandredevv <alexfilho062005@gmail.com>
2026-01-15 20:11:33 -0300

    Add nuqs dependency and integrate NuqsAdapter in layout; implement search functionality in Header component

--- src/app/(board)/header.tsx
+++ src/app/(board)/header.tsx
@@ -1,7 +1,19 @@
+'use client';
+
 import { Input } from '@/components/input';
 import { LogInIcon, SearchIcon } from 'lucide-react';
+import { useQueryState, parseAsString, debounce } from 'nuqs';
 
 export function Header() {
+  const [search, setSearch] = useQueryState('q', parseAsString.withDefault(''));
+
+  function handleSearchUpdate(event: React.ChangeEvent<HTMLInputElement>) {
+    setSearch(event.target.value, {
+      limitUrlUpdates:
+        event.target.value.length !== 0 ? debounce(500) : undefined,
+    });
+  }
+
   return (
     <header className="max-w-[900px] mx-auto w-full flex flex-col md:flex-row  gap-4 items-start md:justify-between md:items-center justify-start">
       <div className="space-y-1">
@@ -19,6 +31,8 @@ export function Header() {
             type="text"
             placeholder="Search for features..."
             className="w-full md:w-[270px] pl-8"
+            value={search}
+            onChange={handleSearchUpdate}
           />
         </div>
 
